fa2950f91117dbc99384d11791e90d490eca2961
ggakila <gisorehaggai@gmail.com>
2023-09-26 18:10:31 +0300

    trial

---
+++ src/Components/Works.js
@@ -0,0 +1,24 @@
+export default function Works() {
+  return (
+		<div className="bg-white h-screen w-full flex flex-col px-[50px]">
+			<h1 className="text-[100px]">Some of my projects.</h1>
+			<div className="flex justify-between w-full">
+				<div className="w-1/2">
+					<div className="">
+						<h1>1. Moments </h1>
+					</div>
+					<div>
+						<div>
+							<h1>2. MovieBox </h1>
+						</div>
+						<h1>3. Duqqa </h1>
+					</div>
+					<div>
+						<h1>4. Bazu Chat  </h1>
+					</div>
+				</div>
+				<div className="w-1/2">right</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Header from "@/Components/Header"
 import About from "@/Components/About";
+import Works from "@/Components/Works";
 
 export default function Home() {
   return (
     <main className="" >
       <Header/>
       <About/>
+      <Works/>
     </main>
   )
 }
