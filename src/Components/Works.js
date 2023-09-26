98ac47ce0a0a7cfabea5b7022fb90953d133af6f
ggakila <gisorehaggai@gmail.com>
2023-09-26 18:21:43 +0300

    pushed

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -3,18 +3,18 @@ export default function Works() {
 		<div className="bg-white h-screen w-full flex flex-col px-[50px]">
 			<h1 className="text-[100px]">Some of my projects.</h1>
 			<div className="flex justify-between w-full">
-				<div className="w-1/2">
-					<div className="">
+				<div className="w-1/2 flex flex-col gap-[50px]">
+					<div className="text-[50px] border">
 						<h1>1. Moments </h1>
 					</div>
 					<div>
-						<div>
 							<h1>2. MovieBox </h1>
 						</div>
+						<div>
 						<h1>3. Duqqa </h1>
 					</div>
 					<div>
-						<h1>4. Bazu Chat  </h1>
+						<h1>4. Bazu Chat </h1>
 					</div>
 				</div>
 				<div className="w-1/2">right</div>
